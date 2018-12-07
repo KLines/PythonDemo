80b8b8641831673b744f7b00fc531405bf3aa5aa
liuk <471636288@qq.com>
2018-12-07 18:39:53 +0800

    code

--- python/基础语法demo.py
+++ python/基础语法demo.py
@@ -1,32 +1,55 @@
-print("=======str list tuple dict 数据类型 的用法========")
-# a =1
-# print(id(1))
+print("======= 1、str list tuple dict 数据类型 的用法 ========")
+# a =100000000
+# b =100000000
+# print(id(100000000))
 # print(id(a))
-String = "hello world"
-List = ['runoob', 786, 2.23, 'john', 70.2]
+# print(id(b))
+String, str = "hello world", "test"
+List = ['runoob', 786, "john", 'john', 70.2, ['test1', 'test2']]  # 元素可以是可变对象
 # 元组不能二次赋值，相当于只读列表
-Tuple = ('tuple', 123, 'test')
-Dictionary = {'name': 'john', 'code': 6734, 'dept': 'sales'}
-print('string :' + String)
+Tuple = ('tuple', 123, 'test', ['test1', 'test2'])  # 元素可以是可变对象
+tuple = ('test1', 'test2')
+Set = {'Tom', 'Mary', 'Tom', 'Jack', 'Rose', tuple}  # 元素必须是不可变对象
+Dictionary = {'name': 'john', tuple: 6734, 'dept': 'sales'}  # key值必须是不可变对象
+print('str : ' + String)
+print('str : ' + str)
 print('修改前 list : ', end="")
 print(List)
 List[2] = 123
 print('修改后 list : ', end="")
-print(List[:])
+print(List[::2]) # step 为 2
 print('tuple : ', end="")
 print(Tuple)
+print('set : ', end="")
+print(Set)
 print('dict : ', end="")
 print(Dictionary)
-# print(Dictionary.get('name')) # 获取对应键的值
-# print(Dictionary['code'])
+# print(Dictionary.get(tuple))  # 获取对应键的值
+# print(Dictionary['tuple'])
 print(Dictionary.keys())  # 输出所有键
 print(Dictionary.values())  # 输出所有值
+print(Dictionary.items())
+print("数据类型：", type(1.0), type(String), type(List), type(Tuple), type(Set), type(Dictionary))
 
-print("=======type isinstance 用法========")
+# 产生随机数
+L = ['b', 'a', 'c']
+L = list(set(L))
+print(L)
 
 
-class A:
-    pass
+# 可变参数
+def test(*args):
+    print(args)
+    return args
+
+
+print(type(test(1, 2, 3, 4)))  # 可以看见其函数的返回值是一个元组
+
+print("======= 2、type isinstance 用法 ========")
+
+
+class A:  # 空类
+    pass  # pass是空语句，是为了保持程序结构的完整性，不做任何事情，一般用做占位语句。
 
 
 class B(A):
@@ -39,7 +62,7 @@ print("type(A()) == A :", type(A()) == A)  # returns True
 print("isinstance(B(), A) :", isinstance(B(), A))  # returns True
 print("type(B()) == A :", type(B()) == A)  # returns False
 
-print("=======and or not 的用法========")
+print("======= 3、and or not 的用法 ========")
 a = 10
 b = 20
 print("a=10,b=20")
@@ -62,7 +85,7 @@ print("a or b :", a or b)
 print("not (a and b) :", not (a and b))
 
 "== 与 is 的区别：is 与 == 区别，is 用于判断两个变量引用对象是否为同一个， == 用于判断引用变量的值是否相等。"
-print("===== is 与 == 的区别========")
+print("===== 4、is 与 == 的区别 ========")
 a = [1, 2, 3]
 b = a  # 引用同一个对象
 print(b is a)  # true
@@ -73,7 +96,7 @@ print(b is a)  # false
 print(id(b) == id(a))  # false
 print(b == a)  # true
 
-print("======= 拷贝和引用的用法 ========")
+print("======= 5、拷贝和引用的用法 ========")
 import copy
 
 dict1 = {'a': [8, 2, 3, 4, 5], 'b': 4}
@@ -98,7 +121,7 @@ print(dict4)
 # 修改动态数据类型，浅拷贝对应的数据发生改变，深拷贝的数据不发生改变
 print('修改动态数据类型')
 for i in range(5):
-    dict3['a'][i] = 0
+    dict2['a'][i] = 0
 print("源字典：", end="")
 print(dict1)
 print("浅拷贝字典：", end="")
@@ -108,7 +131,7 @@ print(dict3)
 print("深拷贝字典：", end="")
 print(dict4)
 
-print("======= if elif 条件语句 ========")
+print("======= 6、if elif 条件语句 ========")
 x = 0
 y = 10
 if (x > 0) and (y / x > 2):  # 采用短路规则
@@ -124,6 +147,7 @@ print(b)
 
 print("使用 switcher 字典映射：")
 
+
 def zero():  # 方法
     return "zero()"
 
@@ -138,7 +162,7 @@ def two():
 
 def num2Str(arg):
     switcher = {
-        0: zero, # 对应函数名
+        0: zero,  # 对应函数名
         1: one,
         2: two,
         3: lambda: "three()"
@@ -162,3 +186,46 @@ if __name__ == '__main__':  #
     print((num(3)))
     print("函数与lambda字典映射：", end="")
     print(num2Str(2))
+
+print("======= 7、while 循环语句 ========")
+
+
+def trim(args):
+    while args[:1] == " ":
+        args = args[1:]
+    while args[-1:] == " ":
+        args = args[:-1]
+    return args
+
+
+str = "  trim  "
+print(trim(str))
+
+print("======= 8、for 循环语句 ========")
+s = 'Python'
+print("当前所有值：", end="")
+for letter in 'Python':
+    print(letter, end="")
+print("")
+
+print("偶数索引值：", end="")
+for index in range(0, len(s), 2):  # 索引值+2
+    print(s[index], end="")
+print("")
+
+print("当前水果：", end="")
+fruits = ['banana', 'apple', 'mango']
+for index, item in enumerate(fruits):  # 打印索引以及对应值
+    # if ('apple' == fruits[index]):
+    #     print(fruits[index], end="")
+    # continue
+    if index == len(fruits) - 1:
+        print(fruits[index])
+    else:
+        print(fruits[index] + ",", end="")
+else:
+    print("当前水果：无")
+
+print(range(1, 3))  # 返回的是一个对象，并没有将数据完全实例化，所以内存中只有一个对象的空间
+print(list(range(1, 3)))
+print(type(list()))
