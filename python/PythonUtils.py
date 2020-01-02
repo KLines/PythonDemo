b62832dfd1afce3e5804ef5c3ead2edf1afb6c50
liuk <liukai@hoperun.com>
2020-01-02 17:44:02 +0800

    删除文件

--- python/PythonUtils.py
+++ python/PythonUtils.py
@@ -1,33 +1,30 @@
+
 import datetime
 from functools import wraps
 
-
 def logUtils(dir="log.txt"):
-    def logging_decorator(func):
+    def logging(func):
         @wraps(func)
-        def wrap_function():
-            func_name = wrap_function.__name__
+        def wrapper():
             with open(dir, "a+") as file:
                 time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                file.write(time + ":\000")
-                file.write(func_name)
-                file.write(wrap_function.__doc__ + "\n")
+                file.write(time+"\n")
+                file.write(wrapper.__name__+" was called\n")
+                file.write(wrapper.__doc__ + "\n")
             func()
-        return wrap_function
-    return logging_decorator
+        return wrapper
+    return logging
 
 
 @logUtils()
 def myFunc1():
     'myFunc1 文档说明'
     print("test1")
-    pass
 
 @logUtils("temp.txt")
 def myFunc2():
     'myFunc2 文档说明'
     print("test2")
-    pass
 
 myFunc1()
 myFunc2()
