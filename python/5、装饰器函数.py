99afdfa4fcbb28213db978b4c994f3cfdf9e9333
liuk <liukai@hoperun.com>
2020-01-02 14:05:41 +0800

    类装饰器

--- python/5、装饰器函数.py
+++ python/5、装饰器函数.py
@@ -5,39 +5,68 @@
 from functools import wraps
 
 # 程序只是简单的将定义代码块读入内存中
-def func(func):
+def func(func): # 闭包
     def wrapper(*args,**kwargs):# 封装内部函数，防止被装饰的函数未进行调用的情况下，程序已经执行完毕
         print("装饰器")
         func(*args,**kwargs)
     return wrapper
 
-print("======= 1、未使用@装饰 ========")
+print("======= 1、装饰器函数-未使用@装饰 ========")
 def origin():
-    print("函数装饰器--被装饰的函数")
+    print("被装饰的函数")
     pass
 origin = func(origin) # 等价于 func(origin)()
 origin()
 
-print("======= 1、使用@装饰 ========")
+print("======= 1、装饰器函数-使用@装饰 ========")
 # @func这句代码在程序执行到这里时会自动执行func函数内部的代码
 @func # 等价于 demo = func(demo)
-def demo():
-    print("函数装饰器--被装饰的函数")
+def demo(name, age):
+    print("被装饰的函数")
+    print(name, age)
     pass
 print(demo.__name__)
-demo() #  只有调用了，才会执行
+demo("test", 21) #  只有调用了，才会执行
 
 
 
-print("======= 2、带参数的装饰器 ========")
+print("======= 2、装饰器函数-两个装饰器装饰一个函数 ========")
+
+def func1(func):
+    print("func1")
+    def wrapper1(*args, **kwargs):
+        print("认证成功")
+        func(*args, **kwargs)
+        print("打印日志")
+    return wrapper1
+
+def func2(func):
+    print("func2")
+    def wrapper2():
+        print("一条欢迎信息")
+        func()
+        print("一条离开信息")
+    return wrapper2
+
+@func2 # test = func2(test)
+@func1 # test = func1(test)
+def test(): # 自下而上的顺序调用装饰器函数 f = func2(func1(f))
+    print("两个装饰器装饰一个函数")
+    pass
+
+print("name：", test.__name__)
+test()
+
+
+print("======= 3、装饰器函数-一般参数 ========")
 
 def logit(args):
-    print(args)
     def logging(func):
         @wraps(func)
-        def wrapper(*args,**kwargs):
+        def wrapper():
+            print(args)
             print(func.__name__+" was called")
-            func(*args, **kwargs)
+            func()
         return wrapper
     return logging
 
@@ -49,14 +78,65 @@ test()
 
 
 
-print("======= 3、类装饰器 ========")
+print("======= 4、装饰器函数-函数名参数 ========")
+
+def outh():
+    print("认证成功")
+
+def log():
+    print("添加日志")
+
+# 装饰器函数。接收两个参数，这两个参数是某个函数的名字
+def filter(func_outh,func_log):
+    # 第一层封装，test函数实际上被传递给了func这个参数
+    def logging(func):
+        # 第二层封装，auth和log函数的参数值被传递到了这里
+        def wrapper(*args,**kwargs):
+            func_outh()
+            func(*args,**kwargs)
+            func_log()
+        return wrapper
+    return logging
+
+# 其实这里 filter(auth,log) = logging , @filter(auth,log) = @logging
+@filter(outh,log)
+def test():
+    print("装饰器函数--参数是函数的装饰器")
+    pass
+test()
+
+
+
+print("======= 5、类装饰器-不带参数 ========")
+
+class Func():
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        print("class start")
+        self.func(*args, **kwargs)
+        print("class end")
+
+@Func
+def test(*args, **kwargs):
+    print(args)
+    print("函数装饰器--类作为装饰器使用")
+    print(kwargs)
+    pass
+
+test("demo", 1, name="test", age=20)
+
+
+
+print("======= 6、类装饰器-带参数 ========")
 
 class Func(object):
     def __init__(self, args):
         self.args = args
-
-    def __call__(self, func):
         print(self.args)
+
+    def __call__(self,func):
         def wrapper(*args, **kwargs):
             print("class start")
             print(args)
@@ -64,98 +144,12 @@ class Func(object):
             print("class end")
         return wrapper
 
-@Func("class args")
+# 装饰器的参数从 __init__ 函数中传
+@Func(args="class args")
 def test(*args, **kwargs):
-    print("函数装饰器--类装饰器")
+    print("函数装饰器--类作为装饰器使用")
     print(kwargs)
     pass
 
+# 函数的参数从 __call__ 函数中传
 test("demo", 1, name="test", age=20)
-
-
-print("======= 4、两个装饰器装饰一个函数 ========")
-
-def func1(func): #将被装饰函数传入
-    print("func1")
-    def wrapper1(*args, **kwargs):
-        print("wrapper1")
-        func(*args, **kwargs)  #执行被装饰的函数
-    return wrapper1  #将装饰完之后的函数返回（返回的是函数名）
-
-def func2(func):
-    print("func2")
-    def wrapper2():
-        print("wrapper2")
-        func()
-    return wrapper2
-
-@func2
-@func1
-def test(): # 自下而上的顺序调用装饰器函数 f = func2(func1(f))
-    print("函数装饰器--两个装饰器装饰一个函数")
-    pass
-
-print("name：", test.__name__)
-test()
-
-
-
-
-
-
-
-#  函数装饰器
-if __name__ == "__main__":
-
-    '函数装饰器 Demo'
-
-    print("====入参 @funcA @funcB ：====")
-    def funcA(A):  # 入参是函数
-        print("funcA")
-
-    def funcB(B):  # 入参是函数
-        print(B(3))
-        print("funcB")
-
-    @funcA  # 等价于 funcA(funcB(func))
-    @funcB
-    def func(c):  # 作为入参传递，无法单独调用
-        print("装饰器传参：func")
-        return c ** 2
-
-    print("====入参 @funcC ：====")
-    def funcC(C):  # 入参是函数
-        @wraps(C)  # 表示不改变入参函数，可以访问入参函数的属性
-        def temp(x, *args):
-            print("function is temp")
-            print(temp.__name__)
-            print(C(x, *args))
-        return temp  # 返回内部函数，用于func在外部可以独立调用
-
-    @funcC   # 使用@修饰函数后 func 变成 funcC 内部返回的函数对象 func = temp
-    def func(x, *args):  # 等价于 funcC(func)(4, test", 2323)
-        """ 使用函数修饰符"""
-        print("function is func", args)
-        return x**2
-
-    print("name :", func.__name__)
-    print("doc :", func.__doc__)
-    func(4, "test", 2323)  # 修饰器下的函数可以独立调用
-
-    print("====入参 @funcD() ：====")
-    def funcD(): #
-        print("function is funcD")
-        def inner(func):
-            print("function is inner")
-            @wraps(func)
-            def temp():
-                func()  # 执行修饰器下的函数
-            return temp
-        return inner
-
-    @funcD()  # 等价于  funcD()(func)()
-    def func():
-        print("function is func")
-
-    func()
-
