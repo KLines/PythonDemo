079b6a8edae86bd80189bc474a35ac11dacfaf81
liuk <471636288@qq.com>
2020-03-16 17:59:31 +0800

    1、增加读取excel数据　 2、增加线程池测试http请求

--- python/auto/ExcelUtils.py
+++ python/auto/ExcelUtils.py
@@ -1,4 +1,6 @@
-import pandas
+import pandas,datetime
+from concurrent.futures import ThreadPoolExecutor
+import network,json
 
 '''
     xlrd：只能读不能写
@@ -7,29 +9,55 @@ import pandas
     https://segmentfault.com/a/1190000018177573
 '''
 
+
 class Excel:
 
     def __init__(self,filename):
-        pass
+        self.filename = filename
+        # 防止空值变成nan
+        self.pd = pandas.read_excel(self.filename,encoding='utf-8',keep_default_na=False)
 
     def read(self):
-        pass
+        self.data_list = []
+        for index in self.pd.index.values:
+            row_data = self.pd.loc[index].to_dict()
+            self.data_list.append(row_data)
 
-    def write(self):
-        pass
+    def write(self,id:int,result:str):
+        self.pd.loc[id-1,'测试结果']=result
+        e.pd.to_excel('test.xlsx',index=False,encoding='utf-8',keep_default_na=False)
 
+index = 0
 
-# if __name__ == '__main__':
+def func(item):
 
-p = pandas.read_excel('test.xlsx',encoding='utf-8')
-print(p)
+    values = list(item.values())
 
+    if 'Y' == values[3].upper():
+        url = values[2]
+        method = values[4]
+        params = values[6]
+        if params!='':
+            params = json.loads(params)
+        if 'GET' == method.upper():
+            network.requests_utils(url,method,params=params)
+        else:
+            network.requests_utils(url,method,json=params)
+        global index
+        index+=1
+        # e.write(item['id'],'Pass')
 
-    # e = Excel('w','test.xlsx')
-    #
-    # e.write()
-    #
-    # e.read()
-    #
-    # for data in e.list_data:
-    #     print(data)
+if __name__ == '__main__':
+
+    # 读取excel数据
+    e = Excel('test.xlsx')
+    e.read()
+    # 开启线程池论询
+    start = datetime.datetime.now()
+    print(start)
+    with ThreadPoolExecutor(max_workers=10,thread_name_prefix="network") as executor:
+        for item in e.data_list:
+            executor.submit(func,item)
+    end = datetime.datetime.now()
+    print(end)
+    print(index)
